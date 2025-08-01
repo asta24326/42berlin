b84dbacd6d663cb06d66f9852e020d1d5805d427
Aidar Sharafeev <asharafe@c4b12c1.42berlin.de>
2025-08-01 22:23:55 +0200

    ft_atol added

---
+++ lib_ft/ft_atol.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asharafe <asharafe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 22:22:25 by asharafe          #+#    #+#             */
+/*   Updated: 2025/08/01 22:23:23 by asharafe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atoi(const char *nptr)
+{
+	int		i;
+	int		sign;
+	long	result;
+	long	digit;
+
+	digit = 0;
+	sign = 1;
+	i = 0;
+	result = 0;
+	while ((nptr[i] >= '\t' && nptr[i] <= '\r') || (nptr[i] == ' '))
+		i++;
+	if ((nptr[i] == '+') || (nptr[i] == '-'))
+		if (nptr[i++] == '-')
+			sign = -1;
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		result = result * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return ((result * sign));
+}
