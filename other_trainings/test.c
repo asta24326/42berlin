c9fa194d48bb8c6a4616b154cbc7dd897179f3bf
Aidar Sharafeev <aidarsharafeev@MacBook-Pro-2.local>
2025-10-08 23:08:45 +0200

    ft_itoa added

--- other_trainings/test.c
+++ other_trainings/test.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int	ft_int_len(int num);
+
 int	main(void)
 {
-	printf("ASCII value of \\0: %d\n", '\0');
+	int	test;
+
+	test = 123;
+	printf("%d, \n", test);
+	printf("%d, \n", ft_int_len(test));
 	return (0);
 }
 
+int	ft_int_len(int num)
+{
+	int	i;
+
+	i = 0;
+	while (num / 10)
+	{
+		i++;
+		num = num / 10;
+	}
+	i++;
+	return(i);
+}
