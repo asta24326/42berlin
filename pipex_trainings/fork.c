a548d5cffa8a2753a134f1adda332de9bd93bb26
Aidar Sharafeev <aidarsharafeev@MacBook-Pro-2.local>
2025-10-18 17:11:15 +0200

    wait added

--- pipex_trainings/fork.c
+++ pipex_trainings/fork.c
@@ -11,20 +11,19 @@ int	main(int argc, char **argv)
 
 	if (ret_value == -1)
 		return (perror("fork failed"), -1);
-	
 	if (ret_value == 0)
 	{
 		printf("I'm a child\n");
 		printf("My parent pid: %d\n", getppid());
 		printf("My pid: %d\n", getpid());
-		return	(0);
+		return (0);
 	}
 	else
 	{
 		printf("I'm a parent\n");
 		printf("My parent pid: %d\n", getppid());
 		printf("My pid: %d\n", getpid());
-		return	(0);
+		return (0);
 	}
 	return (0);
 }
